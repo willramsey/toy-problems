b07f7b158f29ed3a905c09659f90db7f7369a548
William Ramsey <wramsey@gmail.com>
2017-04-22 19:47:02 -0700

    Implement function rockPaperScissors

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -6,6 +6,27 @@
 
 function rockPaperScissors(rounds) {
 // TODO: Your code here
+  var plays = ['rock', 'paper', 'scissors'];
+  var rounds = rounds || 3;
+  var combos = [];
+
+  var generateCombos = function (roundsToGo, playedSoFar) {
+    playedSoFar = playedSoFar || [];
+
+    if (roundsToGo === 0) {
+      combos.push(playedSoFar);
+      return;
+    }
+
+    for (var i = 0; i < plays.length; i++) {
+      var currentPlay = plays[i];
+      generateCombos(roundsToGo - 1, playedSoFar.concat(currentPlay));
+    }
+
+  };
+
+  generateCombos(rounds);
+  return combos;
 }
 
 module.exports = rockPaperScissors;
