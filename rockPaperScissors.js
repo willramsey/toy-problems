1c1f0a32c0bd3d23a2593a2bf15614927c0e5538
William Ramsey <wramsey@gmail.com>
2017-04-22 19:43:32 -0700

    Set up test scaffolding

---
+++ rockPaperScissors.js
@@ -0,0 +1,11 @@
+/*
+* Write a function that generates every sequence of throws a single player
+* could throw for any number of rounds.
+*/
+
+
+function rockPaperScissors(rounds) {
+// TODO: Your code here
+}
+
+module.exports = rockPaperScissors;
---
+++ rockPaperScissors.test.js
@@ -0,0 +1,8 @@
+var rockPaperScissors = require('./rockPaperScissors');
+var should = require('chai').should();
+
+describe('rockPaperScissors', function() {
+  it('should be defined', function() {
+    should.exist(rockPaperScissors);
+  });
+});
