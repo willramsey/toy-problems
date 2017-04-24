5ef5554adb3a2ea61ff53d58cc8c3791642075a5
William Ramsey <wramsey@gmail.com>
2017-04-24 16:43:19 -0700

    Implement function spiralTraversal

--- spiralTraversal.js
+++ spiralTraversal.js
@@ -12,7 +12,43 @@
     returns [1, 2, 3, 6, 9, 8, 7, 4, 5]
  */
 
-var spiralTraversal = function() {};
 
+var spiralTraversal = function(matrix) {
+  var results = [];
+  var startRowIndex = 0;
+  var endRowIndex = matrix.length - 1;
+  var startColIndex = 0;
+  var endColIndex = matrix[0].length - 1;
+
+  while (startRowIndex <= endRowIndex && startColIndex <= endColIndex) {
+
+    for (var i = startColIndex; i <= endColIndex; i++) {
+      results.push(matrix[startRowIndex][i]);
+    }
+    startRowIndex++;
+
+    for (var j = startRowIndex; j <= endRowIndex; j++) {
+      results.push(matrix[j][endColIndex]);
+    }
+    endColIndex--;
+
+    if (startRowIndex <= endRowIndex) {
+      for (var k = endColIndex; k >= startColIndex; k--) {
+        results.push(matrix[endRowIndex][k]);
+      }
+      endRowIndex--;
+    }
+
+    if (startColIndex <= endColIndex) {
+      for (var m = endRowIndex; m >= startRowIndex; m--) {
+        results.push(matrix[m][startColIndex]);
+      }
+      startColIndex++;
+    }
+
+  }
+
+  return results;
+  };
 
 module.exports = spiralTraversal;
