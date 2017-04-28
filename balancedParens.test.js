e99ebca5c681cbe48eafb4ec31a876d6ee4a3461
William Ramsey <wramsey@gmail.com>
2017-04-27 21:18:01 -0700

    Add tests for steps 2 and 3

--- balancedParens.test.js
+++ balancedParens.test.js
@@ -42,4 +42,55 @@ describe('balancedParens', function() {
       balancedParens('(())()(())').should.be.true;
     });
   });
+    describe('step 2', function() {
+    it('should return false for {', function() {
+      balancedParens('{').should.be.false;
+    });
+    it('should return false for }', function() {
+      balancedParens('}').should.be.false;
+    });
+    it('should return true for {}', function() {
+      balancedParens('{}').should.be.true;
+    });
+    it('should return false for [', function() {
+      balancedParens('[').should.be.false;
+    });
+    it('should return false for ]', function() {
+      balancedParens(']').should.be.false;
+    });
+    it('should return true for []', function() {
+      balancedParens('[]').should.be.true;
+    });
+    it('should return true for [](){}', function() {
+      balancedParens('[](){}').should.be.true;
+    });
+    it('should return true for [({})]', function() {
+      balancedParens('[({})]').should.be.true;
+    });
+    it('should return false for [(]{)}', function() {
+      balancedParens('[(]{)}').should.be.false;
+    });
+    it('should return true for [[[{{{((()))}}}]]]', function() {
+      balancedParens('[[[{{{((()))}}}]]]').should.be.true;
+    });
+    it('should return false for []}{()', function() {
+      balancedParens('[]}{()').should.be.false;
+    });
+  });
+  describe('step 3', function() {
+    it('should return true for this string', function() {
+      balancedParens('return { name: "Bertrand Russell", birthday: getBirthday({who:self}) };').should.be.true;
+    });
+    it('should return false for this string', function() {
+      balancedParens(' var hubble = function() { telescopes.awesome();'); // missing trailing `}`
+    });
+    it('should return true for this string', function() {
+      balancedParens(' var wow  = { yo: thisIsAwesome() }').should.be.true;
+    });
+    it('should return true for an empty string', function() {
+      // great job for making it this far and sorry for being a stickler
+      // but technically, the empty string '' is still balanced!
+      balancedParens('').should.be.true;
+    });
+  });
 });
