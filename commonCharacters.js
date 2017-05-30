1208909f19367536678d05c6342b38374918bbec
William Ramsey <wramsey@gmail.com>
2017-05-30 14:00:26 -0700

    Implement function commonCharacters and pass tests

--- commonCharacters.js
+++ commonCharacters.js
@@ -11,9 +11,39 @@
  */
 
 
+var intersection = function (set1, set2) {
+  return Object.keys(set1).reduce(function (out, val) {
+    if (val in set2) { out[val] = true; }
+    return out;
+  }, {});
+};
+// Takes a string and makes an object with each  alphabetical character in the string represented by a key with the value 'true'
+var objectify = function (string) {
+  return string.split('').reduce(function (obj, char) {
+     // this simple regex matches only alphabetical characters of either case
+    if (char.match(/[a-z]/i)) { obj[char] = true; }
+    return obj;
+  }, {});
+};
 
 var commonCharacters = function(string1, string2) {
   // TODO: Your code here!
+
+  // Separate out multiple input strings
+  var otherStrings = Array.prototype.slice.call(arguments, 1);
+
+ // Use reduce to iterate over all collections of letters, narrowing down the pool of common characters.
+ // Go look at the helper functions and figure out what they do!
+  var common = otherStrings.reduce(function(obj, string) {
+    obj = intersection(obj, objectify(string));
+    return obj;
+  }, objectify(string1)); // An object representing all characters in string1 is passed in as a starting value
+
+  // use reduce to create a string representing all common chars in the order seen in string1, and return it!
+  return string1.split('').reduce(function(result, char) {
+    if (common[char]) { result += char; common[char] = false; }
+    return result;
+  }, '');
 };
 
 module.exports = commonCharacters;
